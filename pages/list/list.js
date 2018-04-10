a6ed112bcf08c857c2050454f417ac125f253c56
MinhongShen <761591766@qq.com>
2018-04-10 18:32:20 +0800

    8-4-写箭头

---
+++ pages/list/list.js
@@ -0,0 +1,117 @@
+// pages/list/list.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    img:[
+      {
+        url: 'https://y.gtimg.cn/music/common/upload/iphone_order_channel/toplist_4_300_213470055.jpg?max_age=2592000',
+        num: 4,
+        title: '巅峰榜·流行指数'
+      },
+      {
+        url: 'https://y.gtimg.cn/music/common/upload/iphone_order_channel/toplist_26_300_213465893.jpg?max_age=2592000',
+        num: 26,
+        title: '巅峰榜·热歌'
+      },
+      {
+        url: 'https://y.gtimg.cn/music/common/upload/iphone_order_channel/toplist_27_300_213464582.jpg?max_age=2592000',
+        num: 27,
+        title: '巅峰榜·新歌'
+      },
+      {
+        url: 'https://y.gtimg.cn/music/common/upload/iphone_order_channel/toplist_28_300_200284331.jpg?max_age=2592000',
+        num: 28,
+        title: '巅峰榜·网络歌曲'
+      },
+      {
+        url: 'https://y.gtimg.cn/music/common/upload/iphone_order_channel/toplist_5_300_213452935.jpg?max_age=2592000',
+        num: 5,
+        title: '巅峰榜·内地'
+      },
+      {
+        url: 'https://y.gtimg.cn/music/common/upload/iphone_order_channel/toplist_6_300_213429092.jpg?max_age=2592000',
+        num: 6,
+        title: '巅峰榜·港台'
+      },
+      {
+        url: 'https://y.gtimg.cn/music/common/upload/iphone_order_channel/toplist_3_300_213506475.jpg?max_age=2592000',
+        num: 3,
+        title: '巅峰榜·欧美'
+      },
+      {
+        url: 'https://y.gtimg.cn/music/common/upload/iphone_order_channel/toplist_16_300_212928794.jpg?max_age=2592000',
+        num: 16,
+        title: '巅峰榜·韩国'
+      },
+      {
+        url: 'https://y.gtimg.cn/music/common/upload/iphone_order_channel/toplist_17_300_213290345.jpg?max_age=2592000',
+        num: 17,
+        title: '巅峰榜·日本'
+      },
+      {
+        url: 'https://y.gtimg.cn/music/common/upload/iphone_order_channel/toplist_36_300_200525765.jpg?max_age=2592000',
+        num: 36,
+        title: '巅峰榜·K歌金曲'
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,74 +1,66 @@
+// pages/search/search.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    swiperImg: [
-      'https://y.gtimg.cn/music/photo_new/T003R720x288M000003kGTF00E6oXe.jpg?max_age=2592000',
-      'https://y.gtimg.cn/music/photo_new/T003R720x288M0000008Xxwd1417y8.jpg?max_age=2592000',
-      'https://y.gtimg.cn/music/photo_new/T003R720x288M000002ab5of4Z6qH0.jpg?max_age=2592000',
-      'https://y.gtimg.cn/music/photo_new/T003R720x288M000000Zh3iv2LrsM2.jpg?max_age=2592000',
-      'https://y.gtimg.cn/music/photo_new/T003R720x288M000000i8cxD0fyOo0.jpg?max_age=2592000'
-    ],
-    interval: 3000,
-    duration: 1500,
-    activeColor: '#ccc'
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+  
   }
 })
